01d21da81d9002b4e534d0147b8e22c3113f8fb3
Marco Pugliese <126952052+Marco-Pugliese@users.noreply.github.com>
2023-10-26 16:29:15 +0200

    first commit

---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,61 @@
+import { Component } from "react";
+import { ListGroup } from "react-bootstrap";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+
+class CommentArea extends Component {
+  state = {
+    comments: [],
+    elementId: "",
+  };
+  getComments = () => {
+    fetch(
+      `https://striveschool-api.herokuapp.com/api/comments/${this.state.elementId}`,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTNhNTlhZWY2ZTNkZDAwMTQ5NWU0M2YiLCJpYXQiOjE2OTgzMjU2NTIsImV4cCI6MTY5OTUzNTI1Mn0.rXj_H_Vib2iRPI7SOui6JxJAcOtqeiB2aN93exzpDVY",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Error while recieving Comments");
+        }
+      })
+      .then((datas) => {
+        console.log(datas);
+        this.setState({ comments: datas });
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+  componentDidMount() {
+    this.getComments();
+  }
+
+  render() {
+    return (
+      <ListGroup>
+        <ListGroup.Item className="text-center py-2">
+          <h6 className="py-0 my-0">Commenti: </h6>
+        </ListGroup.Item>
+        {this.state.comments.map((comment) => {
+          return (
+            <div>
+              <CommentList />
+              <ListGroup.Item key={comment.elementId} className="py-1">
+                {comment.comment} - {comment.author} | {comment.rate}
+              </ListGroup.Item>{" "}
+            </div>
+          );
+        })}
+        <AddComment />
+      </ListGroup>
+    );
+  }
+}
+export default CommentArea;
